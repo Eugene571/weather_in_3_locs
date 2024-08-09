2f87eb1bde072194e3f04dc762acbfeaa73d9e69
Eugene Pisarev <zm1s3ry@gmail.com>
2024-08-09 14:25:38 +0300

    updated weather_in_3_locs.py
    Changed variables in case not to cross with project's name.
    Turn ''res'' variable much easier to understand into ''wtr_in_console''

--- weather_in_3_locs.py
+++ weather_in_3_locs.py
@@ -1,12 +1,12 @@
 import requests
 
-locs = [
+places = [
     'london',
     'svo',
     'череповец',
 ]
 
-for loc in locs:
-    res = requests.get('https://ru.wttr.in/' + loc + '?n&lang=ru&?M&?q&?T')
-    res.raise_for_status()
-    print(res.text)
+for place in places:
+    wtr_in_console = requests.get('https://ru.wttr.in/' + place + '?n&lang=ru&?M&?q&?T')
+    wtr_in_console.raise_for_status()
+    print(wtr_in_console.text)
