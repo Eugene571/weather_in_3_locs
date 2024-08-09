1b0f8e9f3b861f34a4f9355aa3d1ba2163923e4b
Eugene Pisarev <zm1s3ry@gmail.com>
2024-08-09 14:45:12 +0300

    added ifmain/def main
    important to have an option to use module as a part of a different programm

--- weather_in_3_locs.py
+++ weather_in_3_locs.py
@@ -1,12 +1,18 @@
 import requests
 
-places = [
-    'london',
-    'svo',
-    'череповец',
-]
 
-for place in places:
-    wtr_in_console = requests.get('https://ru.wttr.in/' + place + '?n&lang=ru&?M&?q&?T')
-    wtr_in_console.raise_for_status()
-    print(wtr_in_console.text)
+def main():
+    places = [
+        'london',
+        'svo',
+        'череповец',
+    ]
+
+    for place in places:
+        wtr_in_console = requests.get('https://ru.wttr.in/' + place + '?n&lang=ru&?M&?q&?T')
+        wtr_in_console.raise_for_status()
+        print(wtr_in_console.text)
+
+
+if __name__ == '__main__':
+    main()
